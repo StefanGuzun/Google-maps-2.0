77693163d4774509e3710cd0de4746be4f945aec
Stefan <stefanguzun2005@gmail.com>
2022-05-21 22:55:01 +0300

    I changed the css of the site

--- javasc.js
+++ javasc.js
@@ -2,9 +2,18 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 47.010452, lng: 28.863810 },
     zoom: 8,
   });
 }
 
 window.initMap = initMap();
+
+const easterEgg = document.querySelector(".easterEgg");
+const easterTxt = document.querySelector(".easterTxt");
+const easterEgg2 = document.querySelector(".easterEggSec");
+
+easterEgg.addEventListener("click", (e) => {
+  easterTxt.innerHTML = "Alexandru";
+  easterEgg2.src = "img/drianmalcolm-696x442.png"
+})

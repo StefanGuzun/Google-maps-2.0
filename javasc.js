97e478eda13b411b3f35961cdc6f1b3b8af89ef7
Iulian <iulian.gonta13@gmail.com>
2022-05-21 22:59:14 +0300

    Added the marker on click

--- javasc.js
+++ javasc.js
@@ -1,10 +1,29 @@
 let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+function placeMarker(location) {
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map
   });
 }
 
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8,
+  });
+
+  google.maps.event.addListener(map, 'click', function(event) {
+    placeMarker(event.latLng);
+  });
+
+  l
+}
+
 window.initMap = initMap();
+
+
+
+
+
